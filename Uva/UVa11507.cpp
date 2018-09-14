ee6c3fa096a8d2d7b0f6998d1d147bc6361dfe54
Ramonr20 <tprog.ramon.rios@outlook.com>
2018-09-14 11:09:45 -0500

    Bender B. Rodriguez Problem - SOLVED

--- Uva/UVa11507.cpp
+++ Uva/UVa11507.cpp
@@ -6,36 +6,39 @@ using namespace std;
 int main() {
 
 	int L;
-	string mov, init = "+x", ans;
-	int i = 0;
-	bool flag, flag2;
-	string arr[100001];
-
-	while (scanf("%d", &L)) { 
-		flag = flag2 = true;
-		
-		while (L--) {	
-			cin >> mov;
-
-			if (mov != "No" && flag) {
-				init = mov;
-				flag = false;
-			}
-
-			if (mov[1] != init[1])
+	string J;
+	string end;
+	while ((cin >> L), L) {
+		end = "+x";
+		for (int i = 1; i < L; ++i) {
+			cin >> J;
+			if (J == "No")
 				continue;
-			else if (flag2) {
-				if (mov[0] == init[0]) {
-					ans = "-x";
-					init = mov;
-				}else {
-					ans = "+x";
-					init = mov;
-				}
-				flag2 = false;
-			}else 
-				ans = mov;
+			else if (J == "+z") {
+				if (end == "+x") end = "+z";
+				else if (end == "-x") end = "-z";
+				else if (end == "+z") end = "-x";
+				else if (end == "-z") end = "+x";
+			} else if (J == "-z") {
+				if (end == "+x") end = "-z";
+				else if (end == "-x") end = "+z";
+				else if (end == "+z") end = "+x";
+				else if (end == "-z") end = "-x";
+
+			} else if (J == "+y") {
+				if (end == "+x") end = "+y";
+				else if (end == "-x") end = "-y";
+				else if (end == "+y") end = "-x";
+				else if (end == "-y") end = "+x";
+
+			} else if (J == "-y") {
+				if (end == "+x") end = "-y";
+				else if (end == "-x") end = "+z";
+				else if (end == "+y") end = "+x";
+				else if (end == "-y") end = "-x";
+			}
 		}
+		cout << end << "\n";
 	}
 	return 0;
 }
